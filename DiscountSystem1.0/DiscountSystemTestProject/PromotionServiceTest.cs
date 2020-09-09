a180b250e2b0df3e01351f32118d4d5cf532c0a5
Sumit Patro <Sumit.Patro@Emerson.com>
2020-09-09 14:21:07 +0530

    Implemented test cases

--- DiscountSystem1.0/DiscountSystemTestProject/PromotionServiceTest.cs
+++ DiscountSystem1.0/DiscountSystemTestProject/PromotionServiceTest.cs
@@ -64,7 +64,7 @@ namespace PromotionSystemTestProject
         }
 
         [Test]
-        public void TestCalculatePromotionDefault()
+        public void TestCalculateTotalPricenDefault()
         {
             try
             {
@@ -82,7 +82,7 @@ namespace PromotionSystemTestProject
         }
 
         [Test]
-        public void TestCalculatePromotionPositive()
+        public void TestCalculateTotalPricenPositive()
         {
             try
             {
@@ -95,12 +95,170 @@ namespace PromotionSystemTestProject
             }
         }
         [Test]
-        public void TestCalculatePromotionNegative()
+        public void TestCalculateTotalPriceNegative()
         {
             try
             {
                 var retVal = promotionSvc.CalculateTotalPrice("A");
                 Assert.AreNotEqual(0, retVal);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestGetPurchaseListDefault()
+        {
+            try
+            {
+                promotionSvc.GetPurchaseList(String.Empty);
+                Assert.Pass();
+            }
+            catch (SuccessException)
+            {
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestGetPurchaseListPositive()
+        {
+            try
+            {
+                var actual = promotionSvc.GetPurchaseList("3A,B");
+                Assert.AreEqual(new List<char>() { 'A','A','A','B'}, actual);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+        [Test]
+        public void TestGetPurchaseListNegative()
+        {
+            try
+            {
+                var actual = promotionSvc.GetPurchaseList("A");
+                Assert.AreNotEqual(new List<char>() { 'B' }, actual);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestGetPromotionSimplifiedDefault()
+        {
+            try
+            {
+                promotionSvc.GetPromotionSimplified();
+                Assert.Pass();
+            }
+            catch (SuccessException)
+            {
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestGetPromotionSimplifiedPositive()
+        {
+            try
+            {
+                var actual = promotionSvc.GetPromotionSimplified();
+                var expected = new Dictionary<List<char>, float>()
+                    {
+                        { new List<char>{ 'A','A','A'}, 2 },
+                        { new List<char>{ 'A','B','C'}, 4 },
+                    };
+
+                Assert.AreEqual(expected, actual);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+        [Test]
+        public void TestGetPromotionSimplifiedNegative()
+        {
+            try
+            {
+                var actual = promotionSvc.GetPromotionSimplified();
+                var expected = new Dictionary<List<char>, float>()
+                    {
+                        { new List<char>{ 'A','A','A'}, 2 },
+                        { new List<char>{ 'A','B','C'}, 3 },
+                    };
+                Assert.AreNotEqual(expected, actual);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestCalculateMinimumPossiblePriceDefault()
+        {
+            try
+            {
+                promotionSvc.CalculateMinimumPossiblePrice(null,0);
+                Assert.Pass();
+            }
+            catch (SuccessException)
+            {
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [Test]
+        public void TestCalculateMinimumPossiblePricePositive()
+        {
+            try
+            {
+                var purchaseList = new List<char>() { 'A' };
+                var simplifiedPromotionList = new Dictionary<List<char>, float>()
+                    {
+                        { new List<char>{ 'A','A','A'}, 2 },
+                        { new List<char>{ 'A','B','C'}, 3 },
+                    };
+                var actual = promotionSvc.CalculateMinimumPossiblePrice(purchaseList, simplifiedPromotionList);
+                Assert.AreEqual(1, actual);
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+        [Test]
+        public void TestCalculateMinimumPossiblePriceNegative()
+        {
+            try
+            {
+                var purchaseList = new List<char>() { 'A' };
+                var simplifiedPromotionList = new Dictionary<List<char>, float>()
+                    {
+                        { new List<char>{ 'A','A','A'}, 2 },
+                        { new List<char>{ 'A','B','C'}, 3 },
+                    };
+                var actual = promotionSvc.CalculateMinimumPossiblePrice(purchaseList, simplifiedPromotionList);
+                Assert.AreEqual(0, actual);
             }
             catch (Exception)
             {
