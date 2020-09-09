1aff4b9e7d29bf74f316e873268dfdc69e560e71
Sumit Patro <Sumit.Patro@Emerson.com>
2020-09-09 12:29:19 +0530

    Added services files and created new test proj

---
+++ DiscountSystem1.0/DiscountSystem1.0/Core/Enums.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscountSystem1._0.Core
+{
+    class Enums
+    {
+    }
+}
---
+++ DiscountSystem1.0/DiscountSystem1.0/Services/IDiscountService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscountSystem1._0.Services
+{
+    class IDiscountService
+    {
+    }
+}
